ff7242c4486ed57cf9f3a32f6f4567fab142c313
frobby-sketch <f.robby@students.amikom.ac.id>
2025-12-18 10:10:20 +0700

    Remove unnecessary whitespace and clean up code

--- Latihan 1.cpp
+++ Latihan 1.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// deklarasi fungsi encrypt
+
 string encrypt(string text);
 
 int main()
@@ -15,15 +15,12 @@ int main()
     return 0;
 }
 
-// fungsi encrypt()
-// mengganti semua huruf vokal dengan '*'
 string encrypt(string text)
 {
     for (int i = 0; i < text.length(); i++)
     {
         char c = text[i];
 
-        // cek huruf vokal (besar dan kecil)
         if (c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o' ||
             c == 'A' || c == 'I' || c == 'U' || c == 'E' || c == 'O')
         {
@@ -32,5 +29,6 @@ string encrypt(string text)
     }
     return text;
 }
+
 
 
