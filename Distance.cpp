ec14d62e2680895cb84ac10c66a0fa1fddd4f441
frobby-sketch <f.robby@students.amikom.ac.id>
2025-12-18 10:02:01 +0700

    Refactor pyramid function and comments
    Fix formatting issues and clean up comments in the pyramid function.

---
+++ Distance.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+void pyramid(int height, int current = 1);
+
+int main()
+{
+    int height = 5;
+    pyramid(height);
+
+    return 0;
+}
+
+void pyramid(int height, int current)
+{
+    
+    if (current > height)
+        return;
+
+    for (int i = 0; i < height - current; i++)
+        cout << " ";
+
+    /
+    for (int i = 0; i < (2 * current - 1); i++)
+        cout << char(178);
+
+    cout << endl;
+
+    pyramid(height, current + 1);
+}
+
